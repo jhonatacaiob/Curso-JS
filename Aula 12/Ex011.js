519141a8e58797ff875904546b53c0165133e059
Jhonata Caio <jhonatacaiob@gmail.com>
2020-12-17 23:57:38 -0300

    Exercicios da aula 12

---
+++ Aula 12/Ex011.js
@@ -0,0 +1,7 @@
+idade = 14
+console.log(`Voce tem ${idade} anos`)
+if(idade < 16){
+    console.log("Nao pode votar")
+}else if(idade < 18 || idade > 65){
+    console.log("Voto opcional")
+}else{"Voto obrigatorio"}
